879fef546655c25b67021ba1cb320762e27eb8ea
HGiang15 <gianghoang150503@gmail.com>
2024-12-24 18:03:59 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./containers/Home/Home";
+
+function App() {
+    return (
+        <div className="App">
+            <Header />
+
+            <Home />
+
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
