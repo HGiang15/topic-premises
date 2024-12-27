10af345d041e122d036a8f9d2e83c7e71756c236
HGiang15 <gianghoang150503@gmail.com>
2024-12-27 11:12:55 +0700

    Updated Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,9 +7,11 @@ const Header = () => {
             <div className="header-container">
                 <a href="/" className="header-logo">N3 Topic</a>
                 <nav className="header-nav">
-                    <ul className="header-nav-list">
+                    <ul className="header-nav-list center-nav">
                         <li className="header-nav-item"><a href="/" className="header-nav-link">Home</a></li>
                         <li className="header-nav-item"><a href="/chatbot" className="header-nav-link">Chatbot</a></li>
+                    </ul>
+                    <ul className="header-nav-list right-nav">
                         <li className="header-nav-item"><a href="/login" className="header-nav-link">Login</a></li>
                         <li className="header-nav-item"><a href="/register" className="header-nav-link">Register</a></li>
                     </ul>
