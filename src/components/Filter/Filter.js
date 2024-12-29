ba6396b70fb5b51c09526ca36b53470abfa885a0
HGiang15 <gianghoang150503@gmail.com>
2024-12-29 11:46:50 +0700

    Done Filter component

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,5 +1,6 @@
 import React from "react";
 import searchIcon from '../../assets/icons/search.svg'
+import arrowDown from '../../assets/icons/arrowDown.svg'
 import "./Filter.css";
 
 const Filter = () => {
@@ -14,32 +15,58 @@ const Filter = () => {
             </div>
             <div className="filter-bar">
                 <div className="filter-options">
-                    <select className="filter-select">
-                        <option value="">Loại hình</option>
-                        <option value="1">Chung cư</option>
-                        <option value="2">Nhà đất</option>
-                    </select>
-                    <select className="filter-select">
-                        <option value="">Mức giá</option>
-                        <option value="1">Dưới 1 tỷ</option>
-                        <option value="2">1-2 tỷ</option>
-                    </select>
-                    <select className="filter-select">
-                        <option value="">Tỉnh/TP</option>
-                        <option value="HCM">Hồ Chí Minh</option>
-                        <option value="HN">Hà Nội</option>
-                    </select>
-                    <select className="filter-select">
-                        <option value="">Quận/Huyện</option>
-                        <option value="1">Quận Long Biên</option>
-                        <option value="2">Quận Đống Đa</option>
-                        <option value="3">Huyện Thanh Trì</option>
-                    </select>
-                    <select className="filter-select">
-                        <option value="">Diện tích</option>
-                        <option value="1">Dưới 50m²</option>
-                        <option value="2">50-100m²</option>
-                    </select>
+                    <div className="custom-select">
+                        <select className="filter-select">
+                            <option value="">Loại hình</option>
+                            <option value="1">Chung cư</option>
+                            <option value="2">Nhà đất</option>
+                        </select>
+                        <img src={arrowDown} alt="Arrow Down" className="dropdown-icon" />
+                    </div>
+                    <div className="custom-select">
+                        <select className="filter-select">
+                            <option value="">Mức giá</option>
+                            <option value="1">Dưới 1 tỷ</option>
+                            <option value="2">1-2 tỷ</option>
+                            <option value="3">3-4 tỷ</option>
+                            <option value="4">5-6 tỷ</option>
+                            <option value="5">7-8 tỷ</option>
+                        </select>
+                        <img src={arrowDown} alt="Arrow Down" className="dropdown-icon" />
+                    </div>
+                    <div className="custom-select">
+                        <select className="filter-select">
+                            <option value="">Tỉnh/TP</option>
+                            <option value="HCM">Hồ Chí Minh</option>
+                            <option value="HN">Hà Nội</option>
+                            <option value="HP">Hải Phòng</option>
+                            <option value="ND">Nam Định</option>
+                            <option value="HY">Hưng Yên</option>
+                        </select>
+                        <img src={arrowDown} alt="Arrow Down" className="dropdown-icon" />
+                    </div>
+                    <div className="custom-select">
+                        <select className="filter-select">
+                            <option value="">Quận/Huyện</option>
+                            <option value="1">Quận Long Biên</option>
+                            <option value="2">Quận Đống Đa</option>
+                            <option value="3">Quận Thanh Xuân</option>
+                            <option value="4">Quận Tây Hồ</option>
+                            <option value="5">Huyện Thanh Trì</option>
+                        </select>
+                        <img src={arrowDown} alt="Arrow Down" className="dropdown-icon" />
+                    </div>
+                    <div className="custom-select">
+                        <select className="filter-select">
+                            <option value="">Diện tích</option>
+                            <option value="1">Dưới 50m²</option>
+                            <option value="2">50-100m²</option>
+                            <option value="3">100-200m²</option>
+                            <option value="4">100-200m²</option>
+                            <option value="5">200-300m²</option>
+                        </select>
+                        <img src={arrowDown} alt="Arrow Down" className="dropdown-icon" />
+                    </div>
                 </div>
             </div>
         </div>
