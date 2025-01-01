d722f35eb074fdfd97877b6d1b5c83706e472f33
Bach293 <bach01299929785@gmail.com>
2025-01-01 21:57:37 +0700

    [Bach] Upload source chatbot ai gemini

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AppRoutes from "./routes/AppRoutes";
 import "./App.css";
+import Chatbot from "./components/Chatbot/Chatbot";
 
 function App() {
     return (
         <div className="App">
             <AppRoutes />
+            <Chatbot />
         </div>
     );
 }
