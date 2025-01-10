aa9efbb6e37d5d313387ceb8abdf9f2b0944abc3
Bach293 <bach01299929785@gmail.com>
2025-01-10 09:02:07 +0700

    update UI dashboard

--- src/containers/Dashboards/Dashboards.js
+++ src/containers/Dashboards/Dashboards.js
@@ -6,23 +6,38 @@ const Dashboards = () => {
 
     useEffect(() => {
         const interval = setInterval(() => {
-            // Gọi API để kiểm tra sự thay đổi
             fetch("http://127.0.0.1:5000/check-changes")
                 .then((response) => response.json())
                 .then((data) => {
                     if (data.changed) {
-                        reloadAllIframes();
+                        updateRelevantIframes(data.tables);
                     }
                 })
                 .catch((error) => console.error("Error checking changes:", error));
         }, 5000); // Quét mỗi 5 giây
-
-        // Xóa interval khi component unmount
+    
         return () => clearInterval(interval);
     }, []);
-
-    const reloadAllIframes = () => {
-        iframeRefs.current.forEach((iframe) => {
+    
+    const updateRelevantIframes = (changedTables) => {
+        // Map bảng thay đổi tới các biểu đồ tương ứng
+        const tableToChartMapping = {
+            post: [0, 2, 3, 4, 5],
+            user: [1, 3],
+        };
+    
+        // Xác định các biểu đồ cần tải lại
+        const chartsToReload = new Set();
+        changedTables.forEach((table) => {
+            const charts = tableToChartMapping[table];
+            if (charts) {
+                charts.forEach((chartIndex) => chartsToReload.add(chartIndex));
+            }
+        });
+    
+        // Tải lại các iframe tương ứng
+        chartsToReload.forEach((index) => {
+            const iframe = iframeRefs.current[index];
             if (iframe) {
                 iframe.src = ""; // Reset src để tải lại iframe
                 iframe.src = iframe.dataset.src; // Gán lại URL từ data-src
@@ -59,7 +74,7 @@ const Dashboards = () => {
                 ></iframe>
             </div>
             <div id="chart-3" className="dashboard-container">
-                <h3>Biều đồ số lượng bài đăng theo từng thể loại cho thuê</h3>
+                <h3>Biều đồ số lượng bài đăng theo từng thể loại</h3>
                 <iframe
                     ref={(el) => (iframeRefs.current[2] = el)}
                     data-src="https://wired-comic-monkfish.ngrok-free.app/superset/explore/p/bAWX7ewzLwE/?standalone=1&height=400"
