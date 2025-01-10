415f4ea06df9519b1ba8588eaa8027ab538c81c2
Nguyễn Ngọc Bách <144202616+Bach293@users.noreply.github.com>
2025-01-10 10:39:02 +0700

    Update Dashboards.js

--- src/containers/Dashboards/Dashboards.js
+++ src/containers/Dashboards/Dashboards.js
@@ -7,7 +7,7 @@ const Dashboards = () => {
     useEffect(() => {
         const interval = setInterval(() => {
             // Gọi API để kiểm tra sự thay đổi
-            fetch("http://127.0.0.1:5000/check-changes")
+            fetch("http://127.0.0.1:5001/check-changes")
                 .then((response) => response.json())
                 .then((data) => {
                     if (data.changed) {
